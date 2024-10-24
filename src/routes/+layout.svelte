d50bc70a6a452658f1116506e16bc21e2c21cfb7
ZachNiederstadt <niederstadtz@gmail.com>
2024-10-24 17:22:17 -0500

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<img class="w-[100px]" src="/logos/prl.jpg" />
+<nav>my cool menu</nav>
+{@render children()}
---
+++ src/routes/schedule/+page.svelte
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="https://fonts.googleapis.com/css?family=Oxanium&display=swap" rel="stylesheet" />
+		<link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+		<link href="./css/main.css" rel="stylesheet" />
+		<title>Document</title>
+	</head>
+
+	<body>
+		<div class="v1_3">
+			<div class="v3_42"></div>
+			<div class="v3_21"></div>
+			<span class="v10_67">Check Instagram Story the day of races for who’s streaming?</span>
+			<div class="v3_44"></div>
+			<span class="v1_6">Standings</span><span class="v1_7">About</span><span class="v1_8"
+				>Schedule</span
+			><span class="v1_9">Sign Up</span><span class="v1_10"
+				>The Fastest Gran Turismo 7 League in North America</span
+			><span class="v1_11">World Series class driving, with close battles and fun rivalries</span>
+			<div class="v10_69"></div>
+			<div class="v40_70"></div>
+			<span class="v1_12">Current and former World Series Drivers involved</span>
+			<div class="v3_20"></div>
+			<div class="v2_2"></div>
+			<div class="v2_3"></div>
+			<div class="v2_4"></div>
+			<div class="v2_5"></div>
+			<div class="v2_6"></div>
+			<div class="v2_7"></div>
+			<span class="v2_8">Links to our Socials</span>
+			<div class="v3_16"></div>
+			<div class="v3_17"></div>
+			<span class="v10_50">Cyrus Cross</span><span class="v10_51">Robby Heck</span><span
+				class="v10_52">Calen Roach</span
+			><span class="v10_53">Mehdi Hafidi</span><span class="v10_54">Dean Heldt</span><span
+				class="v10_55">Alonso Regalado</span
+			>
+			<div class="v10_58"></div>
+		</div>
+	</body>
+</html>
