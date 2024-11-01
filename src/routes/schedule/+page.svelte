ab50eccbde009ec056dfddaad292537c063edfae
ZachNiederstadt <niederstadtz@gmail.com>
2024-11-01 11:25:24 -0500

    nope again

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,4 +1,4 @@
 <div class="m-10 flex justify-center">
 	<!-- svelte-ignore a11y_missing_attribute -->
-	<img src="/static/schedules/gt31.png" alt="schedules" />
+	<img src="/schedules/gt3.avif" alt="Schedule" />
 </div>
